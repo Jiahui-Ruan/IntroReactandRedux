0c45462dbe0a994c7e3fb26d0b0fa54835d7e4eb
JiahuiRuan <jr58@rice.edu>
2017-03-15 18:44:35 -0500

    implement four component of the website

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import YTSearch from 'youtube-api-search'
-import SearchBar from './components/search_bar'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyC0zIhT3OPy0T0-bC5eUwp7y5CL4a5V3Iw';
-
-YTSearch({key: API_KEY, term: 'macbook'}, function(data) {
-	console.log(data);
-})
 
 class App extends Component {
 	constructor(props) {
@@ -14,13 +12,17 @@ class App extends Component {
 
 		this.state = { videos: [] };
 
-		
+		YTSearch({key: API_KEY, term: 'MacPro'}, (videos) => {
+			this.setState({ videos });
+		});
 	}
 
 	render() {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={this.state.videos[0]}/>
+				<VideoList videos={this.state.videos}/>
 			</div>
 		);
 	}
